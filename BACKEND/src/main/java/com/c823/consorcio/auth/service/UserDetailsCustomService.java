b512e3e10b042eadad2c064df1ce319f2d59b62a
agustin-kiryk <agustin.kiryk@gmail.com>
2022-11-20 16:05:01 -0300

    Add IroleRepository

--- BACKEND/src/main/java/com/c823/consorcio/auth/service/UserDetailsCustomService.java
+++ BACKEND/src/main/java/com/c823/consorcio/auth/service/UserDetailsCustomService.java
@@ -1,8 +1,10 @@
 package com.c823.consorcio.auth.service;
 
+import com.c823.consorcio.Entity.RoleEntity;
 import com.c823.consorcio.Entity.UserEntity;
 import com.c823.consorcio.auth.dto.ResponseUserDto;
 import com.c823.consorcio.auth.exception.RepeatedUsername;
+import com.c823.consorcio.mapper.UserMap;
 import com.c823.consorcio.repository.IApartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,6 +17,9 @@ public class UserDetailsCustomService implements UserDetailsService {
 
   @Autowired
   IApartmentRepository iApartmentRepository;
+  @Autowired
+  UserMap userMap;
+  
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -25,7 +30,8 @@ public class UserDetailsCustomService implements UserDetailsService {
     if (iApartmentRepository.findByApartmentNumber(userDto.getApartmentNumber()) != null){
       throw new RepeatedUsername("Repeted Department Number");
     }
-    UserEntity entity =
+    UserEntity entity = userMap.userAuthDtos2Entity(userDto);
+    RoleEntity role =
 
     return null;
   }
