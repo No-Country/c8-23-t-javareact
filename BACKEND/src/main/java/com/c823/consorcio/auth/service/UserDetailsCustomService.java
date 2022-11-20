6603530629b16a302e2e3872a6a558785cc72af9
agustin-kiryk <agustin.kiryk@gmail.com>
2022-11-20 15:58:47 -0300

    Add IApartmentRepository and method findByApartmentNumber

--- BACKEND/src/main/java/com/c823/consorcio/auth/service/UserDetailsCustomService.java
+++ BACKEND/src/main/java/com/c823/consorcio/auth/service/UserDetailsCustomService.java
@@ -1,7 +1,10 @@
 package com.c823.consorcio.auth.service;
 
+import com.c823.consorcio.Entity.UserEntity;
 import com.c823.consorcio.auth.dto.ResponseUserDto;
 import com.c823.consorcio.auth.exception.RepeatedUsername;
+import com.c823.consorcio.repository.IApartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,15 +13,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserDetailsCustomService implements UserDetailsService {
 
+  @Autowired
+  IApartmentRepository iApartmentRepository;
+
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     return null;
   }
 
   public ResponseUserDto save(ResponseUserDto userDto) throws RepeatedUsername {
-    if (IApartmentRepository.findByApartmentNumber(userDto.getApartmentNumber()) != null){
+    if (iApartmentRepository.findByApartmentNumber(userDto.getApartmentNumber()) != null){
       throw new RepeatedUsername("Repeted Department Number");
     }
+    UserEntity entity = 
+
     return null;
   }
 
--- BACKEND/src/main/java/com/c823/consorcio/repository/IUserRepository.java
+++ BACKEND/src/main/java/com/c823/consorcio/repository/IUserRepository.java
@@ -2,9 +2,10 @@ package com.c823.consorcio.repository;
 
 import com.c823.consorcio.Entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IUserRepository extends JpaRepository<UserEntity, Long> {
 
-  UserEntity findAllByApartmentNumber(Long )
 
 }
